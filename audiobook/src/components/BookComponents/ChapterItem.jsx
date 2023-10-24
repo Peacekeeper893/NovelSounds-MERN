40162ba3c52e009c9404629d1b7a8e445a1dfd6a
Navneet <navneetsindhwani009@gmail.com>
2023-10-25 00:08:18 +0530

    reverted back to previous committed ui for mobile devices

--- audiobook/src/components/BookComponents/ChapterItem.jsx
+++ audiobook/src/components/BookComponents/ChapterItem.jsx
@@ -86,7 +86,7 @@ const ChapterItem = ({ num,
         initial={{ opacity: 0, x: -50 }}
         animate={inView ? { opacity: 1, x: 0 } : { opacity: 0.2, x: -10 }}
         transition={{ duration: 0.5 }}
-        className="p-3 pl-6 bg-neutral-100 m-3 dark:bg-d-bg-300 dark:text-white rounded-md"
+        className="hidden md:block p-3 pl-6 bg-neutral-100 m-3 dark:bg-d-bg-300 dark:text-white rounded-md"
       >
         <div className="flex gap-3 align-top">
           <button onClick={onClickHandler}>
@@ -102,6 +102,26 @@ const ChapterItem = ({ num,
         </div>
         {chapterdetails && <div className="dark:text-d-bg-600">{description}</div>}
       </motion.div>
+
+
+      <div
+        className="md:hidden p-3 pl-6 bg-neutral-100 m-3 dark:bg-d-bg-300 dark:text-white rounded-md"
+      >
+        <div className="flex gap-3 align-top">
+          <button onClick={onClickHandler}>
+            {isPlaying === false ? (
+              <BsFillPlayCircleFill className="mt-1" />
+            ) : (
+              <UseAnimations animation={Activity} size={26} color="white" />
+            )}
+          </button>
+          {chapterdetails && <span className="font-bold">{num}. </span>}{' '}
+          {chapterdetails && <span className="font-semibold">{title}</span>}
+          {!chapterdetails && <span className="font-semibold">Chapter {num}</span>}
+        </div>
+        {chapterdetails && <div className="dark:text-d-bg-600">{description}</div>}
+      </div>
+
     </Fragment>
   );
 };
--- audiobook/src/components/BookDisplay.jsx
+++ audiobook/src/components/BookDisplay.jsx
@@ -1,27 +1,48 @@
-
-
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { Fragment } from "react";
+import { motion } from "framer-motion";
 
 const BookDisplay = ({ name, author, bookimg }) => {
+    return (
+        <Fragment>
+            <motion.div
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                whileHover={{ scale: 1.05 }}
+                onTap={{ scale: 0.95 }}
+                className="hidden md:block lg:h-[533px] lg:w-[250px] w-[140px] h-[315px] md:h[425] md:w-[200] my-3"
+            >
+                <div className="h-[75%] mb-2">
+                    <img
+                        src={bookimg}
+                        alt="img"
+                        className="object-fit h-[240px] w-[140px] lg:h-[400px] lg:w-[250px] md:h[375] md:w-[200]"
+                    />
+                </div>
+                <div className="h-[25%] ">
+                    <p className="lg:text-xl md:text-lg text-md mb-2 font-semibold hover:text-cyan-400 dark:hover:text-d-primary-500">
+                        {name}
+                    </p>
+                    <p className="text-sm dark:text-d-bg-600">{author}</p>
+                </div>
+            </motion.div>
 
-  return (
-    <motion.div
-      initial={{ scale: 0.5, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      whileHover={{ scale: 1.05 }}
-      onTap={{ scale: 0.95 }}
-      className='lg:h-[533px] lg:w-[250px] w-[140px] h-[315px] md:h[425] md:w-[200] my-3'
-    >
-      <div className="h-[75%] mb-2">
-        <img src={bookimg} alt="img" className='object-fit h-[240px] w-[140px] lg:h-[400px] lg:w-[250px] md:h[375] md:w-[200]' />
-      </div>
-      <div className="h-[25%] ">
-        <p className='lg:text-xl md:text-lg text-md mb-2 font-semibold hover:text-cyan-400 dark:hover:text-d-primary-500'>{name}</p>
-        <p className='text-sm dark:text-d-bg-600'>{author}</p>
-      </div>
-    </motion.div>
-  )
-}
+            <div className="md:hidden lg:h-[533px] lg:w-[250px] w-[140px] h-[315px] md:h[425] md:w-[200] my-3">
+                <div className="h-[75%] mb-2">
+                    <img
+                        src={bookimg}
+                        alt="img"
+                        className="object-fit h-[240px] w-[140px] lg:h-[400px] lg:w-[250px] md:h[375] md:w-[200]"
+                    />
+                </div>
+                <div className="h-[25%] ">
+                    <p className="lg:text-xl md:text-lg text-md mb-2 font-semibold hover:text-cyan-400 dark:hover:text-d-primary-500">
+                        {name}
+                    </p>
+                    <p className="text-sm dark:text-d-bg-600">{author}</p>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
 
 export default BookDisplay;
